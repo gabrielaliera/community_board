2384a5e6035cf4911781da609cf45a110c8ae7f4
gabrielaliera <gliera2990@gmail.com>
2023-02-21 15:01:37 -0800

    Create card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
+import Card from './components/Card';
 
 const App = () => {
 
   return (
     <div className="App">
-
-      
+      <h1>Food Truck Favorites</h1>
+      <Card event='Birra-Landia' foodType ="Mexica"/>
+      <Card event = "Mysttik Masala" foodType="Indian"/>
+      <Card event = "The Halal Guys" foodType= "Middle Eastern"/>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Card = (props) =>{
     return(
-
+        <div className = "Card">
+            <img src="" alt=""/>
+            <h2>{props.event}</h2>
+            <h3>{props.foodType}</h3> 
+        </div>
     );
 
 }
