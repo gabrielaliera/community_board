e9653aa689457c6bbb0d7837838a787ca80dbf27
gabrielaliera <gliera2990@gmail.com>
2023-05-09 12:07:28 -0700

    Add button to view website

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,8 @@ const Card = (props) =>{
         <div className = "Card">
             <img className="truck" src={props.img} alt={props.alt}/>
             <h2>{props.name}</h2>
-            <h3>{props.cuisine}</h3> 
+            <h3>{props.cuisine}</h3>
+            <a href={props.href}><button>View Website</button></a>
         </div>
     );
 
